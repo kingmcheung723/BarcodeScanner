cc95c479328c0e231046615273b9477a7e5762ac
King Cheung <kingmcheung723@gmail.com>
2015-03-23 01:20:18 +0800

    Scanner added

--- BarcodeScanner/app/src/main/java/com/example/ytw/barcodescanner/MainActivity.java
+++ BarcodeScanner/app/src/main/java/com/example/ytw/barcodescanner/MainActivity.java
@@ -1,19 +1,31 @@
 package com.example.ytw.barcodescanner;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity  {
+
+    private static IntentIntegrator integrator;
+    private String test = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        this.integrator = new IntentIntegrator(this);
+        this.integrator.initiateScan();
     }
 
 
@@ -37,5 +49,38 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        Intent myIntent1 = new Intent(MainActivity.this, MainActivity.class);
+        startActivity(myIntent1);
+
+        if (resultCode == Activity.RESULT_OK) {
+            IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+            if (scanResult != null) {
+                String barcode = scanResult.getContents();
+                TextView barcodeView = (TextView) this.findViewById(R.id.textView_barcode);
+                barcodeView.setText(barcode);
+
+                new AlertDialog.Builder(this)
+                        .setTitle(barcode)
+                        .setMessage(barcode)
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // continue with delete
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // do nothing
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+            }
+        }
+        // else continue with any other code you need in the method
     }
 }
